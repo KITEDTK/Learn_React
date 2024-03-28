20e99a898c5296b4e91e3cad41f0294de2b1065a
kitedt <kite.dtk@gmail.com>
2024-03-28 10:03:56 +0700

    done edit todo

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -18,13 +18,14 @@ class ListTodo extends React.Component {
     });
     toast.success("Success added!!");
   };
-  handleOnChangeEditTodo = (event) => { // gán giá trị mới cho edit todo
-    let editTodoCopy = {...this.state.editTodo}; // vì không thể dùng editTodo.title rồi gán nên p đưa về object 
+  handleOnChangeEditTodo = (event) => {
+    // gán giá trị mới cho edit todo, editTodoTitle
+    let editTodoCopy = { ...this.state.editTodo }; // vì không thể dùng editTodo.title rồi gán nên p đưa về object
     editTodoCopy.title = event.target.value; // đưa về object rồi gán giá trị
     this.setState({
-        editTodo : editTodoCopy 
-    })
-  }
+      editTodo: editTodoCopy,
+    });
+  };
   handleDeleteTodo = (todo) => {
     console.log(todo);
     let currentState = this.state.listTodos;
@@ -35,9 +36,25 @@ class ListTodo extends React.Component {
     toast.success("Success delete!!");
   };
   handleEditTodo = (todo) => {
-    this.setState({
-      editTodo: todo, // gán giá trị cần edit vào edit todo 
-    });
+    let { listTodos, editTodo } = this.state;
+    let isEmptyObj = Object.keys(editTodo).length === 0;
+    if (isEmptyObj === false && editTodo.id === todo.id) {
+      //save
+      let listTodosCopy = [...listTodos];
+      let editTodoId = listTodosCopy.findIndex((item) => item.id === todo.id);
+      listTodosCopy[editTodoId].title = editTodo.title;
+      this.setState({
+        listTodos: listTodosCopy,
+        editTodo: {}, // cập nhật xong thì reset lại
+      });
+    }
+    //edit
+    else {
+      this.setState({
+        editTodo: todo, // gán giá trị cần edit vào edit todo
+      });
+      console.log("edit");
+    }
   };
   render() {
     let { listTodos, editTodo } = this.state;
@@ -55,15 +72,22 @@ class ListTodo extends React.Component {
                 return (
                   <>
                     <div className="todo-child" key={item.id}>
-                      {isEmptyObj === true ? ( // kiểm tra nếu không có giá trị edit nào được chọn 
+                      {isEmptyObj === true ? ( // kiểm tra nếu không có giá trị edit nào được chọn
                         <span>
                           {index + 1} - {item.title}
                         </span>
-                      ) : editTodo === item ? ( // nếu giá trị edit bằng với giá trị bên trong item thì in ra 
+                      ) : editTodo.id === item.id ? ( // nếu giá trị edit.id bằng với giá trị bên trong item idthì in ra
                         <span>
-                          {index + 1} - <input onChange={(event)=>{this.handleOnChangeEditTodo()}} value={editTodo.title} ></input> 
+                          {index + 1} -{" "}
+                          <input
+                            onChange={(event) => {
+                              this.handleOnChangeEditTodo(event);
+                            }}
+                            value={editTodo.title}
+                          ></input>
                         </span>
-                      ) : ( //nếu không phải thì không biến đổi thành input 
+                      ) : (
+                        //nếu không phải thì không biến đổi thành input
                         <span>
                           {index + 1} - {item.title}
                         </span>
@@ -75,7 +99,9 @@ class ListTodo extends React.Component {
                           this.handleEditTodo(item);
                         }}
                       >
-                        edit
+                        {isEmptyObj === false && editTodo.id === item.id
+                          ? "save"
+                          : "edit"}
                       </button>
                       <button
                         className="delete"
